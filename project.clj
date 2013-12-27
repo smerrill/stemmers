4412f6db2973c9e5c0ce0a66a01ebb7ff3d3c3f6
Steven Merrill <steven.merrill@gmail.com>
2013-12-27 16:09:42 -0600

    Add basic working JavaScript build.

--- project.clj
+++ project.clj
@@ -1,5 +1,11 @@
 (defproject stemmers "0.2.1"
   :description "A collection of stemmers for use in search engines and the like."
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2127"]]
+  :plugins [[lein-cljsbuild "1.0.1"]]
+  :cljsbuild {:builds [{:builds []
+                        :source-paths ["target/cljsbuild-crossover"]}]
+              :crossovers [stemmers.core stemmers.porter stemmers.soundex]
+              :source-paths ["target/cljsbuild-crossover"]}
   :dev-dependencies [[marginalia "0.3.2"]
                      [swank-clojure "1.3.0-SNAPSHOT"]])
